032f61983ef2815b6786ec7e0431789d0f5eb287
jungai <juworaphol125@gmail.com>
2019-04-23 22:53:05 +0700

    move location

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import * as jsonServer from 'json-server'
-import { mapRoutes } from './routes'
+import { mapRoutes } from './app/routes'
 
 const server = jsonServer.create()
-const router = jsonServer.router('../db/db.json')
+const router = jsonServer.router('./db/db.json')
 
 const middlewaresOptions: jsonServer.MiddlewaresOptions = {
     bodyParser: true,
