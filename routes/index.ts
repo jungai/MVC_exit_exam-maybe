7303c98cfdacf014e0671d1aa00be22d21bedf44
jungai <juworaphol125@gmail.com>
2019-04-24 20:58:17 +0700

    change route to / 📭

--- routes/index.ts
+++ routes/index.ts
@@ -2,9 +2,10 @@ import { Application } from 'express'
 import { User } from '../controller/user'
 
 function allRoutes(r: Application) {
-  // add new route here
-  r.get('/users', User)
+  /**
+   * add route here
+   */
+  r.get('/', User)
 }
 
 export const mapRoutes = (s: Application ) => allRoutes(s)
-
