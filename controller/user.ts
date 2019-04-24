0007818efc8277e2fd74772f5d4443da7fb4bf5e
jungai <juworaphol125@gmail.com>
2019-04-24 20:57:53 +0700

    json to render

--- controller/user.ts
+++ controller/user.ts
@@ -3,5 +3,5 @@ import { getUser } from '../model/user'
 
 export const User = (_req: Request, res: Response, _next: NextFunction) => {
   const users = getUser()
-  res.status(200).json({ msg: 'hello', result: users })
+  res.render('index', { msg: 'test mvc', result: users})
 }
